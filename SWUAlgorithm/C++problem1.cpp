b76694623eec1f7c8f05e9d55287f454622c8528
7IEH <80614927+7IEH@users.noreply.github.com>
2021-08-24 20:47:25 +0900

    Add files via upload

---
+++ SWUAlgorithm/C++problem1.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include<string>
+
+std::string longestword(std::string text) {
+    std::string temp;
+    std::string res;
+
+    for (int i = 0; i < text.size(); i++) {
+        if (('a' <= text[i] && text[i] <= 'z') || ('A' <= text[i] && text[i] <= 'Z')) {
+            temp += text[i];
+        }
+        else {
+            if (res.size() < temp.size()) {
+                res = temp;
+            }
+            temp = "";
+        }
+        
+    }
+    if (res.size() < temp.size()) {
+        res = temp;
+    }
+
+    return res;
+}
+
+int main()
+{
+    std::string text;
+    std::getline(std::cin, text);
+    std::cout << longestword(text);
+
+    return 0;
+}
